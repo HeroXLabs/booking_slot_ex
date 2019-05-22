2686f345a192baa6b06bdf32fe18d0989aa761b4
Lin He <he9lin@gmail.com>
2019-05-21 21:14:29 -0700

    Refactor

--- lib/booking_slot/day_slot.ex
+++ lib/booking_slot/day_slot.ex
@@ -7,23 +7,25 @@ defmodule BookingSlot.DaySlot do
     %__MODULE__{id: id}
   end
 
-  def from_time(%Time{} = time) do
+  def from_time(time, min_per_slot \\ 15)
+
+  def from_time(%Time{} = time, min_per_slot) do
     slot_num =
       time
       |> get_total_minutes()
-      |> to_slot_num()
+      |> to_slot_num(min_per_slot)
 
     {:ok, %__MODULE__{id: slot_num}}
   end
 
-  def from_time(time_str) when is_binary(time_str) do
+  def from_time(time_str, min_per_slot) when is_binary(time_str) do
     with {:ok, time} <- Time.new(time_str) do
-      from_time(time)
+      from_time(time, min_per_slot)
     end
   end
 
-  defp to_slot_num(minutes) do
-    Kernel.round(Float.floor(minutes / 15))
+  defp to_slot_num(minutes, min_per_slot) do
+    Kernel.round(Float.floor(minutes / min_per_slot))
   end
 
   defp get_total_minutes(%Time{hour: hour, minute: minute}) do
